5a789b3b8d98858b79ff7fc9b7a104c4e08df1c5
github username <github email address>
2020-05-27 19:39:36 -0700

    Fixed the action cable problems. It's all working now

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,7 +1,19 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
-    conversation = Conversation.find(params[:conversation])
-    stream_for conversation
+    conversation = Conversation.find(params[:id])
+    stream_from "conversation_channel_#{conversation.id}"
+  end
+
+  def receive(data)
+    @user = User.find(JWT.decode(data["user_id"], 'secret', true, algorithm: 'HS256')[0]["user_id"])
+    @conversation = Conversation.find(data["conversation_id"])
+    
+    if @user
+      if @conversation.users.include?(@user)
+        @message = Message.create(text: data["text"], user_id: @user.id, conversation_id: @conversation.id)
+        ActionCable.server.broadcast("conversation_channel_#{@conversation.id}", @message)
+      end
+    end
   end
 
   def unsubscribed
