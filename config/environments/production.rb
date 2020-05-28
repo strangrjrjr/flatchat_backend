ad694d7a199a46d30214edb093371fe469dc27da
Roger Robertson Jr <rjrjr@pm.me>
2020-05-28 16:10:59 -0700

    redis and websockets live

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+
+  config.web_socket_server_url = 'ws://flatchat-demo.herokuapp.com/cable'
+
+  config.action_cable.allowed_request_origins = ['https://flatchat-demo.herokuapp.com', 'http://flatchat-demo.herokuapp.com']
+
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
